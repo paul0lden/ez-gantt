3675bf657896158459c91c40778b06a2f0401daa
Paul Oldenburger <paul4olden@gmail.com>
2024-09-24 02:50:12 +0200

    feat: add barrel for package

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
-import CssBaseline from '@mui/material/CssBaseline'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import EzGantt from './Gantt'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-).render(
-  <React.StrictMode>
-    <CssBaseline />
-    <App />
-  </React.StrictMode>,
-)
+export default {
+  EzGantt,
+}
+
