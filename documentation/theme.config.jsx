0763062514b95d92ed558719c21a5b2512072f3f
Paul Oldenburger <paul4olden@gmail.com>
2024-09-24 23:10:53 +0200

    feat: add docs base setup

--- documentation/theme.config.jsx
+++ documentation/theme.config.jsx
@@ -1,7 +1,19 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>ez-gantt</span>,
   project: {
-    link: 'https://github.com/shuding/nextra'
-  }
-  // ... other theme options
+    link: 'https://github.com/paul0lden/ez-gantt',
+  },
+  docsRepositoryBase: 'https://github.com/paul0lden/ez-gantt/tree/main/documentation/',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – ez-gantt',
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="ez-gantt" />
+      <meta property="og:description" content="Fast and easy to use react gantt chart" />
+    </>
+  ),
 }
